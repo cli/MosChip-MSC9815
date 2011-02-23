a8640b10a1ea422ccc6686241d33b7922392c918
Christian Lins <120872+cli@users.noreply.github.com>
2011-02-23 14:15:22 +0100

    Add ops function comments.

--- src/mcs9815_ops.c
+++ src/mcs9815_ops.c
@@ -8,12 +8,14 @@ extern struct mcs9815_port* port1;
 #define PORT(p) \
 	(p == port0->port ? port0 : port1)
 
+/* Writes the given byte value in the data register */
 void write_data(struct parport* port, unsigned char value)
 {
 	printk(KERN_DEBUG "%s: write_data\n", port->name);
 	outb(value, REG_EPPDATA(PORT(port)));
 }
 
+/* Writes up to len bytes of the given buffer in the specified port */
 size_t compat_write_data(struct parport* port, const void* buf, size_t len, int flags)
 {
 	size_t n;
@@ -26,26 +28,45 @@ size_t compat_write_data(struct parport* port, const void* buf, size_t len, int 
 	return n;
 }
 
+/* Reads a byte off the data register of the given port */
 unsigned char read_data(struct parport* port)
 {
 	printk(KERN_DEBUG "%s: read_data\n", port->name);
 	return inb(REG_DPR(PORT(port)));
 }
 
-void write_control(struct parport* port, unsigned char value)
+/* 
+ * Writes the bitmask control into the control register of the specified
+ * port. The control value is buffered in the mcs9815_port struct for
+ * read access (see read_control, frob_control).
+ */
+void write_control(struct parport* port, unsigned char control)
 {
 	struct mcs9815_port* p = PORT(port);
 	printk(KERN_DEBUG "%s: write_control\n", port->name);
-	outb(value, REG_DCR(p));
-	p->ctrl = value;
+	outb(control, REG_DCR(p));
+	p->ctrl = control;
 }
 
+/* 
+ * Returns the bitmask representing the contents of the port's control
+ * register. As the value is buffered in the mcs9815_struct to actual
+ * hardware access is performed.
+ */
 unsigned char read_control(struct parport* port)
 {
 	printk(KERN_DEBUG "%s: read_control\n", port->name);
 	return PORT(port)->ctrl;
 }
 
+/*
+ * This function takes the contents of the port's control register,
+ * masks out the bits using (negated) mask and xor'ing the result with
+ * val. The result of this operation is written to the port's control
+ * register.
+ * The function can be used to easily toggle bits in the control 
+ * register of the port.
+ */
 unsigned char frob_control(struct parport* port, unsigned char mask,
 							unsigned char val)
 {
@@ -61,12 +82,17 @@ unsigned char frob_control(struct parport* port, unsigned char mask,
 	return p->ctrl;
 }
 
+/* Returns the value of the status register */
 unsigned char read_status(struct parport* port)
 {
 	printk(KERN_DEBUG "%s: read_status\n", port->name);
 	return inb(REG_DSR(PORT(port)));
 }
 
+/*
+ * Reads up to len bytes into the given buffer using parport nibble 
+ * mode (reading status register).
+ */
 size_t nibble_read_data(struct parport* port, void* buf, size_t len, int flags)
 {
 	int n;
@@ -78,6 +104,9 @@ size_t nibble_read_data(struct parport* port, void* buf, size_t len, int flags)
 	return n;
 }
 
+/*
+ * Reads up to len bytes into the given buffer using byte mode.
+ */
 size_t byte_read_data(struct parport* port, void* buf, size_t len, int flags)
 {
 	size_t n;
@@ -90,6 +119,9 @@ size_t byte_read_data(struct parport* port, void* buf, size_t len, int flags)
 	return n;
 }
 
+/*
+ * Sets bit 4 of control register to enable the generation of interrupts.
+ */
 void enable_irq(struct parport* port)
 {
 	printk(KERN_DEBUG "%s: enable_irq\n", port->name);
@@ -98,6 +130,10 @@ void enable_irq(struct parport* port)
 	frob_control(port, (1 << 4), (1 << 4));
 }
 
+/*
+ * Clears bit 4 of the control register to disable interrupts on the
+ * port.
+ */
 void disable_irq(struct parport* port)
 {
 	printk(KERN_DEBUG "%s: disable_irq\n", port->name);
@@ -106,7 +142,9 @@ void disable_irq(struct parport* port)
 	frob_control(port, (1 << 4), 0);
 }
 
-// This sets the data direction to forward (which is default for SPP)
+/*
+ * This sets the data direction to forward (which is default for SPP)
+ */
 void data_forward(struct parport* port)
 {
 	printk(KERN_DEBUG "%s: data_forward\n", port->name);
@@ -115,7 +153,9 @@ void data_forward(struct parport* port)
 	frob_control(port, (1 << 5), 0);
 }
 
-// Reverses the data direction (required for byte transfer mode)
+/*
+ * Reverses the data direction (required for byte transfer mode)
+ */
 void data_reverse(struct parport* port)
 {
 	printk(KERN_DEBUG "%s: data_reverse\n", port->name);
@@ -124,6 +164,9 @@ void data_reverse(struct parport* port)
 	frob_control(port, (1 << 5), (1 << 5));
 }
 
+/*
+ * Initializes the chip.
+ */
 void init_state(struct pardevice* pardev, struct parport_state* parstate)
 {
 	printk(KERN_INFO "mcs9815: init_state\n");
@@ -135,6 +178,9 @@ void init_state(struct pardevice* pardev, struct parport_state* parstate)
 	// Write 0x35 in ECR register for EPP mode
 }
 
+/*
+ * Saves the state of the port. Why is this called on driver removal?
+ */
 void save_state(struct parport* parport, struct parport_state* parstate)
 {
 	printk(KERN_DEBUG "%s: save_state\n", parport->name);
@@ -142,11 +188,17 @@ void save_state(struct parport* parport, struct parport_state* parstate)
 	
 }
 
+/*
+ * Restores the state of the port.
+ */
 void restore_state(struct parport* parport, struct parport_state* parstate)
 {
 	printk(KERN_DEBUG "%s: restore_state\n", parport->name);
 }
 
+/*
+ * Writes the up to len bytes of the given buffer using EPP data registers.
+ */
 size_t epp_write_data(struct parport* port, const void* buf, size_t len, int flags)
 {
 	int n;
